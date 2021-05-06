7fbbc45a82bcdc9feb51ab198fa1968360d1dd8f
Olga Bursina <bursina.olya@yandex.ru>
2021-05-06 12:05:00 +0500

    Добавлено задание 4: запуск IE и Firefox.

---
+++ SeleniumTest/Task4_IE.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace SeleniumTest
+{
+    public class Task4
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private const string loginPage = "http://localhost/litecart/admin/login.php";
+        
+        [SetUp]
+        public void Setup()
+        {
+            driver = new InternetExplorerDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+        
+        [Test]
+        // Проверка заголовка страницы авторизации
+        public void PageTitleIsMyStore()
+        {
+            driver.Url = loginPage;
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+        
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
